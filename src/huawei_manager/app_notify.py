139f953419861bdba6e29b382c735bfa13225591
VDCSS <victordavi13jupiter@gmail.com>
2026-08-04 16:40:16 -0300

    fix(app): dispose session factory and non-blocking shutdown on close

--- src/huawei_manager/app_notify.py
+++ src/huawei_manager/app_notify.py
@@ -5,9 +5,13 @@ class NotifyMixin:
     def _cleanup_executors(self) -> None:
         for pool in (self._io_executor, self._cpu_executor):
             if pool is not None:
-                pool.shutdown(wait=True)
+                pool.shutdown(wait=False)
 
     def _on_close(self) -> None:
+        if getattr(self, "_adaptive_timer", None) is not None:
+            self._adaptive_timer.stop()
+        if getattr(self, "_session_factory", None) is not None:
+            self._session_factory.dispose()
         self._watcher.shutdown()
         self._sb.disconnect()
         self._cleanup_executors()
